9e1ff872dd48ac466ec5ca610681399ae82e3012
Davide Ieria <davideieria1099@gmail.com>
2024-05-08 17:03:36 +0200

    order rating

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,18 +6,40 @@ import Heading from './Components/Heading';
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [order, setOrder] = useState(true);
 
-  const fetchMovies = () => {
+  const fetchMovies = async () => {
     setLoading(true);
 
-    return fetch('http://localhost:8000/movies')
+    await fetch('http://localhost:8000/movies')
       .then(response => response.json())
       .then(data => {
         console.log(data);
-        setMovies(data.sort());
-        setLoading(false);
+        setMovies(data);
       });
+    setLoading(false);
   }
+  const fetchRating = async () => {
+    setLoading(true);
+    setMovies([])
+    await fetch('http://localhost:8000/movies')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        if (order) {
+          setOrder(!order)
+          return setMovies(data.sort(function (a, b) {
+            return b.rating - a.rating
+          }));
+        }
+        setOrder(!order)
+        return setMovies(data.sort(function (a, b) {
+          return a.rating - b.rating
+        }));
+      });
+    setLoading(false);
+  }
+
 
   useEffect(() => {
     fetchMovies();
@@ -26,10 +48,15 @@ export default function App() {
   return (
     <Layout>
       <Heading />
+      <div className='bg-slate-800 mb-4 w-full h-[30px]'>
+        <button onClick={() => fetchRating(!order)} className='bg-green-600 rounded p-2' >
+          {order ? <p> rating ^</p> : <p>rating v</p>}
+        </button>
+      </div>
       <MovieList loading={loading} movies={movies} />
     </Layout>
   );
-};
+}
 
 
 
